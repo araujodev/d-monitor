5de1530c4f54789f9a5a1df74743cff6d2cd1182
Leandro Araujo <leandro.agenciamit@gmail.com>
2025-06-11 18:01:33 -0300

    test asset

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+\Livewire\Livewire::setScriptRoute(function($handle) {
+    return Route::get(config('app.assetUrl') . '/livewire/livewire.js', $handle);
+});
+
+\Livewire\Livewire::setUpdateRoute(function($handle) {
+    return Route::get(config('app.assetUrl') . '/livewire/update', $handle);
+});

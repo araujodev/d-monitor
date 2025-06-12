dbc976c92d063558a5c1a765bd947323671638ab
Leandro Araujo <leandro.agenciamit@gmail.com>
2025-06-11 21:06:06 -0300

    test 4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Livewire\Livewire;
+
+Livewire::setScriptRoute(function ($handle) {
+    $base = env('LIVEWIRE_BASE_PATH');
+    return Route::post(
+        is_null($base) ? null : rtrim($base, '/') . '/livewire.js',
+        $handle
+    );
+});
+
+Livewire::setUpdateRoute(function ($handle) {
+    $base = env('LIVEWIRE_BASE_PATH');
+    return Route::post(
+        is_null($base) ? null : rtrim($base, '/') . '/update',
+        $handle
+    );
+});
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
